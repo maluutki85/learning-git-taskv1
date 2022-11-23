fab8ebf9c491cce66484a9dc5477162e06fd47d9
Jakub Dziurgot <j.dziurgot@heating.pl>
2022-11-23 21:34:12 +0100

    Init project

--- learning-git-taskv1.py
+++ learning-git-taskv1.py
@@ -1,11 +1,1 @@
-shop_list = {
-    "piekarnia": ["chleb", "bułki", "pączek", "bagetka"],
-    "warzywniak": ["marchew", "seler", "rukola", "ogórek"]
-}
-sum = 0
-for name, product_list in shop_list.items():
-    print(f"Idę na zakupy do: {name.capitalize()}, i kupuję tam następujące produkty: {[product.capitalize() for product in product_list]}")
-    sum += len(product_list)
-print(f"W sumię kupuję {sum} produktów")
 
-print("Maciek to jest gość ale nieźle daje w kość")
