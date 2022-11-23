03f80e3a89e2c0ae8156512056ba1200a59e8775
Jakub Dziurgot <j.dziurgot@heating.pl>
2022-11-23 21:54:56 +0100

    Add greetings

--- learning-git-taskv1.py
+++ learning-git-taskv1.py
@@ -7,3 +7,5 @@ for name, product_list in shop_list.items():
     print(f"Idę na zakupy do: {name.capitalize()}, i kupuję tam następujące produkty: {[product.capitalize() for product in product_list]}")
     sum += len(product_list)
 print(f"W sumię kupuję {sum} produktów")
+
+print("Maciek to jest gość ale nieźle daje w kość")
