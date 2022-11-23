f8fe23ffd1dd1a061bdbaa9e01858a46ffa5ade3
Jakub Dziurgot <j.dziurgot@heating.pl>
2022-11-23 21:37:50 +0100

    Add first sentence

--- learning-git-taskv1.py
+++ learning-git-taskv1.py
@@ -2,3 +2,6 @@ shop_list = {
     "piekarnia": ["chleb", "bułki", "pączek", "bagetka"],
     "warzywniak": ["marchew", "seler", "rukola", "ogórek"]
 }
+sum = 0
+for name, product_list in shop_list.items():
+    print(f"Idę na zakupy do: {name.capitalize()}, i kupuję tam następujące produkty: {[product.capitalize() for product in product_list]}")
